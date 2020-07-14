8b03e4fd77f8c1de23d8f0ed7cad9d7581471e8a
JuanB0609 <68257002+JuanB0609@users.noreply.github.com>
2020-07-13 20:32:20 -0400

    Add files via upload

---
+++ POO Colores/POO Colores/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_Colores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            startComponent();
+        }
+
+        private void startComponent()
+        {
+            comboBox1.Items.Add("Rojo");
+            comboBox1.Items.Add("Azul");
+            comboBox1.Items.Add("Verde");
+
+            comboBox1.SelectedIndexChanged += (s, e) =>
+            {
+                int index = comboBox1.SelectedIndex;
+                BackColor = index == 0 ? Color.Red : index == 1 ? Color.Blue : Color.Green;
+            };
+        }
+    }
+}
