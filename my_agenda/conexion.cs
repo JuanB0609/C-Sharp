448438af4abb87e82cf2761ab9f449104506dbfd
JuanB0609 <68257002+JuanB0609@users.noreply.github.com>
2020-07-16 17:29:09 -0400

    Add files via upload

---
+++ my_agenda/conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_agenda
+{
+    class conexion
+    {
+        public static SQLiteConnection conectar()
+        {
+            string database = Application.StartupPath + "\\myagenda.db;";
+            SQLiteConnection cn = new SQLiteConnection("datasource = " + database);
+            return cn;
+        }
+    }
+}
